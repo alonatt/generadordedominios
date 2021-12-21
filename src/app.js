d78be34872e0ece6221c047fc6419503316ed8d0
alonatt <alondra08@ug.uchile.cl>
2021-12-21 01:01:16 +0000

    mirepo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".cl"];
+  let siteName = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j in adj) {
+      for (let k in noun) {
+        for (let l in domain) {
+          siteName.push(`${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`);
+          console.log(siteName);
+          // for (let n in siteName) {
+          //   let randomName = Math.floor(Math.random() * n);
+          //   document.getElementById("domain").innerHTML = siteName[randomName];
+          // }
+        }
+      }
+    }
+  }
 };
